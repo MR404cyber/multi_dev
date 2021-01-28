b47d4b85e94ff8f55fb67ce189145c474c38c930
Iqbal_Dev <47203081+IqbalDev@users.noreply.github.com>
2021-01-28 19:32:56 +0700

    Update multi_dev.py

--- multi_dev.py
+++ multi_dev.py
@@ -140,9 +140,9 @@ if __name__ == '__main__':
 
 		elif pil== '3':
 			try:
-				subprocess.check_output(['am', 'start', 'https://youtu.be/k6BxejPFU4Q'])
+				subprocess.check_output(['am', 'start', 'https://youtu.be/QTR6uFF1Ap8'])
 			except KeyboardInterrupt:
-				subprocess.check_output(['am', 'start', 'https://youtu.be/k6BxejPFU4Q'])
+				subprocess.check_output(['am', 'start', 'https://youtu.be/QTR6uFF1Ap8'])
 			except WindowsError:
 				os.system('multi_dev.py' if os.name == 'nt' else 'python2 multi_dev.py')
 
